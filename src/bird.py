9993a541fc2ee11b1c76a578d2ca8443e7e6bd91
hali26 <hali26@ocdsb.ca>
2022-03-16 18:02:17 -0400

    Bird jumps and moves
    Bird class created, has functions to jump, move, and get drawn on the board.
    
    Background applied to the screen

---
+++ src/bird.py
@@ -0,0 +1,41 @@
+import pygame
+import os
+
+class Bird:
+    ACCELERATION = 3.0 #Value for how fast the bird should accelerate downwards
+    JUMP_CONST = -10.0 #Value for how much the bird should accelerate up by when it jumps
+    TERMINAL_VELOCITY = 16.0 #Value for the amount of displacement where the bird stops accelerating
+
+    def __init__(self, x, y, img = pygame.image.load(os.path.join("Assets","FBird_mediumwings.png"))):
+        self.img = img
+        self.x = x
+        self.y = y
+        self.height = self.y
+        self.velocity = 0.0
+        self.tick = 0
+        self.rect = pygame.Rect(self.x, self.y, img.get_width(), img.get_height())
+        
+    #def update(self):
+
+    def jump(self):
+        self.velocity = self.JUMP_CONST
+        self.tick = 0 #To restart the acceleration
+        self.height = self.y
+        
+    def move(self):
+        self.tick += 1
+
+        #d = vt + 0.5(at^2) kinematic equation, getting displacement of y for the movement of the bird
+        displacement = self.velocity*(self.tick) + (0.5)*(self.ACCELERATION)*(self.tick)**2
+        print(displacement)
+
+        if displacement >= self.TERMINAL_VELOCITY:
+            displacement = self.TERMINAL_VELOCITY if displacement > 0 else (-1)*(self.TERMINAL_VELOCITY) #Stays at T.V with the correct sign
+
+        if displacement < 0:
+            displacement -= 2 #If it jumped, make the acceleration upwards slowly turn downwards
+
+        self.y = self.y + displacement
+        self.rect.y = self.y
+    def draw(self, window):
+        window.blit(self.img, (self.rect.x, self.rect.y))
--- src/main.py
+++ src/main.py
@@ -1,32 +1,45 @@
+from turtle import bgpic
 import pygame
 import os
-
+from bird import Bird
 pygame.init()
 
-SCREEN_HEIGHT = 1536  # mobile height for 8bit games
-SCREEN_WIDTH = 864  # mobile width for 8 bit games
+SCREEN_HEIGHT = 800  # mobile height for 8bit games
+SCREEN_WIDTH = 600  # mobile width for 8 bit games
+
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Flappy Bird AI")
-background = pygame.image.load("../assets/Background Day.png")
+BG =  pygame.transform.scale(pygame.image.load(os.path.join("Assets","bgday.png")).convert_alpha(), (600, 900))
 
-BIRD_FLYING = pygame.image.load("../assets/FBird_mediumwings.png")
+BIRD_FLYING = pygame.image.load(os.path.join("Assets","FBird_mediumwings.png"))
 
+birds = [Bird(60,60)]
 
 def main():
-    pygame.mixer.music.load("../audio/music_zapsplat_easy_cheesy.mp3")
-    pygame.mixer.music.play(-1)
-    pygame.mixer.music.set_volume(0.2)
-
+    #pygame.mixer.music.load("../audio/music_zapsplat_easy_cheesy.mp3")
+    # pygame.mixer.music.play(-1)
+    # pygame.mixer.music.set_volume(0.2)
+    clock = pygame.time.Clock()
     running = True
     while running:
-
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if running == False:
                 pygame.quit()
-            pygame.time.Clock()
+        
+        SCREEN.blit(BG, (0,0))
+        for bird in birds:
+            bird.move()
+            bird.draw(SCREEN)
 
+        user_input = pygame.key.get_pressed()
+
+        for i, bird in enumerate(birds):
+            if user_input[pygame.K_SPACE]:
+                bird.jump()
+        pygame.display.update()
     pygame.quit()  # make sure this stays at the end of our file
 
 
