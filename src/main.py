34641ec9f350dcb0afb27c51fb905a63a452404b
hali26 <hali26@ocdsb.ca>
2022-03-18 20:35:31 -0400

    Collisions works
    Any collision with the pipes leads to gameOver()

--- src/main.py
+++ src/main.py
@@ -16,9 +16,19 @@ BG =  pygame.transform.scale(pygame.image.load(os.path.join("Assets","bgday.png"
 
 birds = [Bird(60,60)]
 opipes = Pipes(600)
+
+def gameOver():
+    #pygame.quit()
+    SCREEN.blit(BG, (0,0))
+
+#Super ugly, will clean up
 def hasCollided(bird, pipes):
-    if pipes.x <= bird.x and bird.x <= (pipes.x + 104): #If the bird is within the width of the pipe
-        if pipes.ytop <=  bird.y and
+    if (pipes.x <= bird.x or pipes.x <= (bird.x+51)) and (bird.x <= (pipes.x + 104) or (bird.x+51) <= (pipes.x + 104)): #If the bird is within the width of the pipe
+        if (pipes.ytop <=  bird.y or pipes.ytop <=  (bird.y + 36)) and (bird.y <= (pipes.ytop + 640) or (bird.y + 36) <= (pipes.ytop + 640)):
+            return True
+        if (pipes.ybot <=  bird.y or pipes.ybot <=  (bird.y + 36)) and (bird.y <= (pipes.ybot + 640) or (bird.y + 36) <= (pipes.ybot + 640)):
+            return True
+        return False
 
 def main():
     #pygame.mixer.music.load("../audio/music_zapsplat_easy_cheesy.mp3")
@@ -46,7 +56,8 @@ def main():
         for i, bird in enumerate(birds):
             if user_input[pygame.K_SPACE]:
                 bird.jump()
-        
+        if hasCollided(bird, opipes):
+            gameOver()
         pygame.display.update()
     pygame.quit()  # make sure this stays at the end of our file
 
